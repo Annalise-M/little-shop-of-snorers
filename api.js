818915a30fbc79f49fdc1374e3593646c6f2e42f
Annalise Murphy <nali247@gmail.com>
2020-04-13 17:47:04 -0700

    step 1

---
+++ api.js
@@ -0,0 +1,21 @@
+export const myData = [
+
+    {
+        id: 'baby-sweep',
+        image: '../assets/sweep.png',
+        name: 'Sweeping Baby',
+    },
+
+    {
+        id: 'shark-attack',
+        image: '../assets/shark.jpg',
+        name: 'Shark Attack',
+    },
+
+    {
+        id: 'tenticle',
+        image: '../assets/usb.gif',
+        name: 'Tenticle Arm',
+    },
+
+];
