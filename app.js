c7c30e194bd0a3a9d3553423bf680a9a790e2860
Annalise Murphy <nali247@gmail.com>
2020-04-13 18:35:11 -0700

    to pass travis

--- app.js
+++ app.js
@@ -1,2 +1,22 @@
 import { myDataArray } from './myDataArray';
 
+// Need Id, Image, Name, Radio
+const idTags = document.querySelectorAll('id');
+const imageTags = document.querySelectorAll('img');
+const nameTags = document.querySelectorAll('name');
+const myRadioTags = document.querySelectorAll('input');
+// const data = new dataArray('myDataArray');
+
+let myWins = 0;
+let correctAnswer;
+
+myRadioTags.forEach((radioTag)) => {
+    radioTag.addEventListener('click', (event) => {
+        if (event.target.value === correctAnswer.id) {
+            myWins++;
+            data = remove
+
+        }
+    }
+    
+};
