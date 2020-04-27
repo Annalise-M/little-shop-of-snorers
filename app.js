59f16a52a1cc45e31cfb0dd0a8b753a3f4cb91e3
Annalise Murphy <nali247@gmail.com>
2020-04-26 21:46:35 -0700

    latest additions

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ const gameCounter = 25;
     //keep track of total clicks, id's clicked and within a 25 click limit + reset after
 let timesClicked = 0;
 
-
 const product1 = pickRandomProduct(productData);
 const product2 = pickRandomProduct(productData);
 const product3 = pickRandomProduct(productData);
@@ -30,17 +29,13 @@ productImg1.src = product1.image;
 productImg2.src = product2.image;
 productImg3.src = product3.image;
 
-//for event click listener
+// ^ prep for event click listener
 
-
-productRadioTag3.value = product3.id;
- //creating blank array to track views and clicks
+//creating blank array to track views and clicks
 const initializeNewProducts = () => {
     const product1 = pickRandomProduct(productData);
     let product2 = pickRandomProduct(productData);
     let product3 = pickRandomProduct(productData);
-    
-    
     
     while (product1.id === product2.id) {
         product2 = pickRandomProduct(productData);
@@ -52,40 +47,52 @@ const initializeNewProducts = () => {
     const product1Object = newProduct1;
     product1Object.textContent = product1Object.name;
     const product1Image = document.getElementById('productImg1');
-    productImg1.src = product1.image;
+    productImg1.src = product1Object.image;
+    const radioButton1 = document.getElementById('product1');
     productRadioTag1.value = product1.id;
-    const product1Name.textContent = product1Object.name;
+    const product1Name = document.getElementById('product1Name');
+    product1Name.textContent = product1Object.name;
 
     const product2Object = newProduct2;
     product2Object.textContent = product2Object.name;
     const product2Image = document.getElementById('productImg2');
-    productImg2.src = product2.image;
+    productImg2.src = product2Object.image;
+    const radioButton2 = document.getElementById('product2');
     productRadioTag2.value = product2.id;
-    const product2Name.textContent = product2Object.name;
-
+    const product2Name = product2Object.name;
+    product2Name.textContent;
+    
     const product3Object = newProduct3;
     product3Object.textContent = product3Object.name;
     const product3Image = document.getElementById('productImg3');
-    productImg3.src = product3.image; 
+    productImg3.src = product3Object.image; 
+    const radioButton3 = document.getElementById('product3');
     productRadioTag3.value = product3.id;
-    const product3Name.textContent = product3Object.name;
+    const product3Name = product3Object.name;
+    product3Name.textContent;
+
+};
+
+function renderProductsResults(){
     
     let answersArray = [];
     let allTimeArrayStorage = localStorage.getItem('allTimeAnswersArrayKey');
     if (allTimeArrayStorage) {
         allTimeArrayStorage = parsed_AllTimeArrayStorage;
     }
-    else { allTimeArray = []; }
-        // //we need to increment the times seen for these three products, cause now we've seen them
-    // incrementTimesSeen(productId, incrementTimesPicked);
-    // incrementTimesPicked(productId, incrementTimesPicked);
+    else { 
+        allTimeArray = [];
+        let allTimeArrayStorage;
+    };
+    // //we need to increment the times seen for these three products, cause now we've seen them
+    incrementTimesSeen(productId, incrementTimesPicked);
+    incrementTimesPicked(productId, incrementTimesPicked);
     saveToLocalStorage(productData);
     localStorage.setItem('results', newResultsState);
     // gameCounter--;
-
+    
     saveToLocalData();
     renderItems();
-    
 };
 
 initializeNewProducts();
@@ -95,14 +102,39 @@ nextButton.addEventListener('click', () => {
     const productId = selectedRadioButton.value;
     console.log(productId);
     renderProductsResults();
-    //now we need to increment this id
-    //then we need to stringify the array then store in local storage
-    //then we need to create 3 more products  /
+    const possibleResults = localStorage.getItem('results');
+    let storageResults;
+    if (possibleResults) {
+        storageResults = JSON.parse(possibleResults);
+    }
+    else {
+        storageResults = [];
+        
+    }
+    const productSelected = document.querySelector('input:clicked');
+    let productChoiceResults = findById(storageResults, productSelected.value);
     
+    //now we need to increment this id
+    if (!productChoiceResults){
+        const selectedResultsObject = {
+            id: productSelected.value,
+            votes: 1
+        };
+        storageResults.push(selectedResultsObject);
+    } else {
+        productChoiceResults.votes++;
+    }
+    //then we need to stringify the array then store in local storage
+    const newResultsState = JSON.stringify(storageResults);
+    
+    localStorage.setItem('results', newResultsState);
+}
+    //then we need to create 3 more products  /
 
-});
+);
+console.log(localStorage);
+
 //putting + tracking user info into local storage
-
 export function saveUser(user) {
     const json = JSON.stringify(user);
     localStorage.setItem('user', json);
--- utils.js
+++ utils.js
@@ -1,89 +1,71 @@
-/* eslint-enable */
-// import { productData } from './productData.js';
 
-// //random generator
-// export function pickRandomProduct(array) {
-//     const randomIndex = Math.floor(Math.random() * 20);
-//     return array[randomIndex];
-// }
+import { productData } from './productData.js';
 
-// // // function findById
-// // export function findById(productData, productId){
-// //     for (let i = 0; i < productData.length; i++) {
-// //         if (selectedGood[i].id === productId) {
-// //             return selectedGood;
-// //         }
-// //     }
-// //     return null;
-// }
+//random generator
+export function pickRandomProduct(array) {
+    const randomIndex = Math.floor(Math.random() * 20);
+    return array[randomIndex];
+}
 
-// // export function incrementTimesClicked(id, insertObject){
-// //     const clickedItem = findById(insertObject, id);
-// //     // is this id in this array?
-// //     if (!incrementTimesClicked) {
-// //         addInitialEventListener
+// function findById
+export function findById(productData, productId){
+    for (let i = 0; i < productData.length; i++) {
+        if (selectedGood[i].id === productId) {
+            return selectedGood;
+        }
+    }
+    return null;
+}
 
-// //     }
-// //     // if so, incrementtimes clicked ++
-// //     //if not, then insert object function
-// // }
-// // export function incrementTimesSeen()
-
-// // function productRadioTag
-
-// // function timesClicked
-// // // function gameCounter
-
-// // export function incrementTimesSeen() {
-// //     let voteItem = findById(productData, id);
+export function incrementTimesSeen() {
+    let voteItem = findById(productData, id);
   
-// //     if (!voteItem) {
-// //         addInitialVoteItem(id, productData);
-// //         voteItem = findById(productData, id);
-// //     }
+    if (!voteItem) {
+        addInitialVoteItem(id, productData);
+        voteItem = findById(productData, id);
+    }
   
-// //     voteItem.timesSeen++;  
-
-//     // Not sure about this function...??
-// }
+    voteItem.timesSeen++;  
+}
   
-// export function incrementTimesPicked(id, productData) {
-//     let voteItem = findById(productData, id);
+export function incrementTimesPicked(id, productData) {
+    let voteItem = findById(productData, id);
   
-//     if (!voteItem) {
-//         addInitialVoteItem(id, productData);
-//         voteItem = findById(productData, id);
-//     }
+    if (!voteItem) {
+        addInitialVoteItem(id, productData);
+        voteItem = findById(productData, id);
+    }
   
-//     voteItem.votes++;
-// }
+    voteItem.votes++;
+}
   
-// function addInitialVoteItem(randomObj, array) {
-//     const voteItem = {
-//         id: randomObj.id,
-//         timesViewed: 0,
-//         votes: 0
+export function addInitialVoteItem(randomObj, array) {
+    const voteItem = {
+        id: randomObj.id,
+        timesViewed: 0,
+        votes: 0
     
-//     };
+    };
   
-//     array.push(voteItem);
-//     const stringed = JSON.parsed(array);
-//     localStorage.setItem('chartData', stringed);
-// }
+    array.push(voteItem);
+    const stringed = JSON.parsed(array);
+    localStorage.setItem('chartData', stringed);
+}
 
-// // function seedProductsIntoLocalStorage(){
-// //     const productsAlreadyExist = localStorage.getItem('results');
+function seedProductsIntoLocalStorage(){
+    const productsAlreadyExist = localStorage.getItem('results');
 
-// //     if (!productsAlreadyExist){
-// //         const stringyProducts = JSON.stringify(productsData);
-// //         localStorage.setItem('results', stringyProducts);
-// //     }
-// }
+    if (!productsAlreadyExist){
+        const stringyProducts = JSON.stringify(productsData);
+        localStorage.setItem('results', stringyProducts);
+    };
+};
 
-// export function getProducts(){
-//     //seeding products into local storage
-//     const storedProducts = localStorage.getItem('results');
-//     const parsedProducts = JSON.parse(storedProducts);
 
-//     return parsedProducts;
-// }
+export function getProducts(){
+    //seeding products into local storage
+    const storedProducts = localStorage.getItem('results');
+    const parsedProducts = JSON.parse(storedProducts);
+
+    return parsedProducts;
+}
